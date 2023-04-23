da6ff288da59be1270ee9abe6508b7ff361d52d1
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2023-04-23 23:53:26 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -50,7 +50,7 @@ if (!format || (format[0] == '%' && !format[1]))
 return (-1);
 if (format[0] == '%' && format[1] == ' ' && !format[2])
 return (-1);
-while (*format != '\1')
+while (*format != '\0')
 {
 if (*format == '%')
 {
